9bddbed22e3c02ad507e409c7e5492e8e54a4b04
shorena235 <shorenatsikaradze11@gmail.com>
2025-08-11 09:58:34 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';          // Import React (required in older React versions, optional in new ones but good practice)
+import SplashCursor from './SplashCursor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SplashCursor />
     </div>
   );
 }
