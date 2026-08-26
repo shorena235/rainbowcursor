a5ed89b1da701249bc9f863ed19c7c7d5cf21e62
Shorena Tsikaradze <58514678+shorena235@users.noreply.github.com>
2026-08-26 23:14:13 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import SplashCursor from './SplashCursor';
 function App() {
   return (
     <div>
-      <SplashCursor />
+<SplashCursor
+  DENSITY_DISSIPATION={3.5}
+  VELOCITY_DISSIPATION={2}
+  PRESSURE={0.1}
+  CURL={3}
+  SPLAT_RADIUS={0.2}
+  SPLAT_FORCE={6000}
+  COLOR_UPDATE_SPEED={10}
+  SHADING
+  RAINBOW_MODE={false}
+  COLOR="#FFFFFF"
+/>
     </div>
   );
 }
